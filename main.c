880a2e32fbc21ac2982feac7b6c6a1876790ef08
fullop <parkth32@gmail.com>
2022-01-24 17:18:35 +0900

    UPDATE div function
    div 함수 구현 추가

--- main.c
+++ main.c
@@ -54,6 +54,10 @@ int multi(int number1, int number2)
  
 int div(int number1, int number2)
 {
-	//TODO implementation
-	return 0;
+	if(number2 == 0)
+	{
+		printf("DIV/0\n");
+		return 0;
+	}
+	return number1 / number2;
 }
